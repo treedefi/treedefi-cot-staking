363ea2bc891dce4f98c62a15b99578d6eb1726c5
hashdevnet <107400085+hashdevnet@users.noreply.github.com>
2023-04-19 14:48:54 +0200

    start smart contract dev

---
+++ contracts/COTStakingInitializable.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract SmartChefInitializable is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20Metadata;
+
+}
