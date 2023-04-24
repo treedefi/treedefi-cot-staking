a6b90ba93fbc18b30eacf3b3500268c3cdbb678b
hashdevnet <107400085+hashdevnet@users.noreply.github.com>
2023-04-24 14:46:18 +0200

    wip: NatSpec Format and code edits

--- contracts/COTStakingInitializable.sol
+++ contracts/COTStakingInitializable.sol
@@ -8,8 +8,14 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+/// @title COT Staking Contract
+/// @notice A contract for staking COT tokens and earning rewards.
 contract COTStakingInitializable is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20Metadata;
+    using SafeMath for uint256;
+
 
     IERC20Metadata public cotToken;
     uint256 public poolSize;
@@ -34,6 +40,13 @@ contract COTStakingInitializable is Ownable, ReentrancyGuard {
     event Unstaked(address indexed user, uint256 amount);
     event RewardClaimed(address indexed user, uint256 amount);
 
+    /// @notice Initializes the staking contract.
+    /// @param cotToken_ The address of the COT token.
+    /// @param poolSize_ The total size of the staking pool.
+    /// @param blockRewardRate_ The rate at which rewards are earned per block.
+    /// @param minStackingLockTime_ The minimum lock time for staking.
+    /// @param poolDuration_ The duration of the staking pool.
+    
     function initialize(
         address cotToken_,
         uint256 poolSize_,
@@ -94,8 +107,8 @@ function unstake() external nonReentrant {
     /* internal functions */
 
     function _calculateReward(uint256 amount, uint256 startBlock, uint256 endBlock) private view returns (uint256) {
-        uint256 blocksStaked = endBlock - startBlock;
-        uint256 reward = (amount * blockRewardRate * blocksStaked) / poolDuration;
+        uint256 blocksStaked = endBlock.sub(startBlock);
+        uint256 reward = amount.mul(blockRewardRate).mul(blocksStaked).div(poolDuration);
         return reward;
     }
     
@@ -108,5 +121,6 @@ function unstake() external nonReentrant {
     function getUserStake(address user) external view returns (Stake memory) {
         return _stakes[user];
     }
+
 
 }
