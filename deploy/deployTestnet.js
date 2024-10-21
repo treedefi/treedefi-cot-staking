614ef8c569a5e92d244133df4c19de1671b9d753
mascarock <niccolo.mas@gmail.com>
2024-10-21 18:57:16 -0500

    dep testnet

--- deploy/deployTestnet.js
+++ deploy/deployTestnet.js
@@ -8,13 +8,13 @@ async function main() {
   const setContractAddress = "0x0FaB8A58793873f1E8adDe7B87065c8bd20d9096";
   const whitelistContractAddress = "0xa00f384c95B3507558316E10a9FD03f17700f2B3";
   const blockNumber = await ethers.provider.getBlockNumber();
-  const blockStartNumber = 31867000 // TESTNET - estimated block number for Mon, 17 July 2023
+  const blockStartNumber = 38158300 // TESTNET - estimated block number for Mon, 17 July 2023
 
   const poolSize = ethers.utils.parseEther("10000");
   const rewardRate = 10; // 20% per year APR (bnb chain) [15-20-25]
   const minStackingLockTime = 40; // 40 blocks = 2 minutes (bnb chain)
   const poolDuration = 10512000;  // 10512000 blocks = 1 year
-  const maxStakePerUser = ethers.utils.parseEther("250"); // 250 COT
+  const maxStakePerUser = ethers.utils.parseEther("250"); // 250 SET
  
 
   const COTStakingInitializable = await ethers.getContractFactory("TreedefiCOTStakingUpgradeable");
